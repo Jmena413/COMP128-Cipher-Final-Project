ad7769491d5787af30af10af476c475a97f6354f
Joseph Mena Lopez <60200285+Trashu413@users.noreply.github.com>
2020-10-11 19:43:15 -0400

    added in caesar cipher encryption and decryption methods.

--- src/CypherDecryption.java
+++ src/CypherDecryption.java
@@ -1,2 +1,28 @@
 public class CypherDecryption {
+
+    String caesarCipher(String message, int offset){
+        StringBuilder result = new StringBuilder();
+        for (char character: message.toCharArray()) {
+            if (character != ' ') {
+                int originalAlphaPos = character - 'a';
+                int newAlphaPos = (originalAlphaPos + offset) % 26;
+                char newCharacter = (char) ('a' + newAlphaPos);
+                result.append(newCharacter);
+            }else{
+               result.append(character);
+            }
+        }
+        return result.toString();
+    }
+
+    String caesarDecipher(String cipheredMessage, int offset){
+        return caesarCipher(cipheredMessage, 26 - (offset % 26));
+    }
+
+    public static void main(String[] args) {
+        CypherDecryption cypher = new CypherDecryption();
+        String newmessage = cypher.caesarCipher("he told me i could never teach a llama to drive", 3);
+        System.out.println(newmessage);
+        System.out.println(cypher.caesarDecipher(newmessage, 3));
+    }
 }
