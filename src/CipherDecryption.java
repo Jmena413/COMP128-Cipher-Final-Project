7644cc4408bbca26b15aa72ff586aa834ae8dba0
Joseph Mena Lopez <60200285+Trashu413@users.noreply.github.com>
2020-10-12 19:15:59 -0400

    class decomposition

--- src/CipherDecryption.java
+++ src/CipherDecryption.java
@@ -3,5 +3,14 @@ import java.util.*;
 public class CipherDecryption {
 
     public static void main(String[] args) {
+        CaesarCipher caesar = new CaesarCipher();
+        SubstitutionCipher substitution = new SubstitutionCipher();
+        String message = caesar.encrypt("attack at dawn", 5);
+        System.out.println(message);
+        System.out.println(caesar.decrypt(message,5));
+        String key = substitution.shuffleAlphabet();
+        message = substitution.encrypt("attack at dawn", key);
+        System.out.println(message);
+        System.out.println(substitution.decrypt(message,key));
     }
 }
