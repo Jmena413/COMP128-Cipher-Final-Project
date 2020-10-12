32cfcd8956d4f7d2e0d8ecfcc079900008f4edcb
Joseph Mena Lopez <60200285+Trashu413@users.noreply.github.com>
2020-10-11 20:53:04 -0400

    added in a simple substitution cipher encryption and decryption method.

--- src/CypherDecryption.java
+++ src/CypherDecryption.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class CypherDecryption {
 
     String caesarCipher(String message, int offset){
@@ -19,10 +22,53 @@ public class CypherDecryption {
         return caesarCipher(cipheredMessage, 26 - (offset % 26));
     }
 
+    String simpleSubstitutionCipher(String message, String key){
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        Map<Character, Integer> alphabetMap = new HashMap<>();
+        int count = 0;
+        for (char character: alphabet.toCharArray()) {
+            alphabetMap.put(character, count);
+            count++;
+        }
+        char[] keyArray = key.toCharArray();
+        StringBuilder result = new StringBuilder();
+        for (char character : message.toCharArray()){
+            if (character != ' ') {
+                result.append(keyArray[alphabetMap.get(character)]);
+            }else{
+                result.append(character);
+            }
+        }
+        return result.toString();
+    }
+
+    String simpleSubstitutionDecipher(String cipheredMessage, String key){
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        Map<Character, Integer> keyMap = new HashMap<>();
+        int count = 0;
+        for (char character : key.toCharArray()){
+            keyMap.put(character, count);
+            count++;
+        }
+        char[] alphabetArray = alphabet.toCharArray();
+        StringBuilder result = new StringBuilder();
+        for (char character : cipheredMessage.toCharArray()){
+            if (character != ' ') {
+                result.append(alphabetArray[keyMap.get(character)]);
+            }else{
+                result.append(character);
+            }
+        }
+        return result.toString();
+    }
+
     public static void main(String[] args) {
         CypherDecryption cypher = new CypherDecryption();
-        String newmessage = cypher.caesarCipher("he told me i could never teach a llama to drive", 3);
+        String newmessage = cypher.caesarCipher("attack at dawn", 25);
         System.out.println(newmessage);
-        System.out.println(cypher.caesarDecipher(newmessage, 3));
+        System.out.println(cypher.caesarDecipher(newmessage, 25));
+        newmessage = cypher.simpleSubstitutionCipher("attack at dawn", "phqgiumeaylnofdxjkrcvstzwb");
+        System.out.println(newmessage);
+        System.out.println(cypher.simpleSubstitutionDecipher(newmessage, "phqgiumeaylnofdxjkrcvstzwb"));
     }
 }
